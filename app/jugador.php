667dc03815d6d0fafa7f6fc73208847f8d5b4bc6
mariana2323 <anaacosiri@gmail.com>
2018-10-23 23:23:12 -0500

    modelo jugador

--- app/jugador.php
+++ app/jugador.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class jugador extends Model
 {
     //
+    public $timestamps = false; //sobre escribe en false el timestamp que esta heredando
+    //Con los sig comando se accede a la base de datos
+    //protected $table = 'minombredetabla';
+    //protected $primaryKey = 'nombreidpersolanizado';
+    protected $fillable = ['id_jugadores', 'apodo', 'nombre', 'apellido', 'fechaNac'];
 }
