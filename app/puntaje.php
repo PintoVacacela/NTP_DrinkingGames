a063b504f3fea63df5257674800d1876b7a048e0
mariana2323 <anaacosiri@gmail.com>
2018-10-25 21:46:09 -0500

    modelo puntaje

--- app/puntaje.php
+++ app/puntaje.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class puntaje extends Model
 {
     //
+    public $timestamps = false; //sobre escribe en false el timestamp que esta heredando
+    //Con los sig comando se accede a la base de datos
+    //protected $table = 'minombredetabla';
+    //protected $primaryKey = 'nombreidpersolanizado';
+    protected $fillable = ['id_jugadores', 'puntaje_cartas', 'puntaje_dados', 'puntaje_ruleta'];
 }
