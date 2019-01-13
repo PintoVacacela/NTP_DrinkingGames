dbf3fc8b18e8f0f8428a5676e3c8844fb260f355
mariana2323 <anaacosiri@gmail.com>
2019-01-13 13:03:35 -0500

    make it better

--- app/Http/Controllers/ruletaDP.php
+++ app/Http/Controllers/ruletaDP.php
@@ -4,26 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ruleta;
+
 //use App\puntajes;
 class ruletaDP extends Controller
 {
-    function retornarUsuario($id){
-        $jugador = ruleta:: where ("id",$id);
+    public $giro = false;
+    public $pin;
+
+    function retornarUsuario($id)
+    {
+        $jugador = ruleta:: where("id", $id);
         return $jugador;
     }
-    function actualizarPuntaje(Request $request,$id)
+
+    function setGiro(Request $request)
     {
-        $valor=25;
+        $giro = $request->input('giro');
+        return Array('success' => true);
+    }
+    function getGiro(){
+        if (isset($giro)) {
+            return Array('giro' => $giro);
+        }
+        else
+            return Array('success' => false);
+    }
+    function actualizarPuntaje(Request $request, $id)
+    {
+        $valor = 25;
         $ruleta = new ruleta;
         $puntaje = new puntaje;
         $usuario = puntaje::find($id);
         $temp = $request->input('boton');
         $puntuacion = $usuario->puntaje;
-        if($temp == 'yes')
-        {
+        if ($temp == 'yes') {
             $puntuacion += this()->$valor;
             $usuario->puntaje = $puntuacion;
             $usuario->save();
         }
+    }
+    function pin ($id){
+        $pin = $id;
+        if (isset($pin)) {
+            return Array('pin' => $pin);
+        }
+        else
+            return Array('success' => false);
     }
 }
